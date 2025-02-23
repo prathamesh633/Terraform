e0b7d2a1a0c5c6c66eb10b3e9644f672b86fdd43
prathamesh633 <prathmeshbhujade1234@gmail.com>
2025-02-23 11:38:19 +0530

    added port

--- main.tf
+++ main.tf
@@ -34,6 +34,12 @@ module "EC2" {
       to_port     = 22
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   ]
 }
